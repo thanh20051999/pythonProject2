c1b804b2cc3b4ed00da6237874c460db7d1e78d2
than <thanhtran.20051999@gmail.com>
2021-03-04 14:47:41 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# s= input("Input your score: ")
+# f= float(s)
+# print(f)
+# if f >= 80:
+#     print("You got A!", end='')
+# elif f >= 70:
+#     print("You got B!")
+# elif f >= 60:
+#     print("You got C!")
+# else:
+#     print("failed")
+print(10/3)
+print(10//3)
+print(10**3)
+print(10*3)
